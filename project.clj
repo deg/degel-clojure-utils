919e8b70035fd179982b61726877d4655f35c31e
David Goldfarb <deg@degel.com>
2013-08-11 13:17:39 +0300

    No longer need to use cljx (at least for now?). Also some minor other cleanups to project.clj. Bump version to 0.1.10.

--- project.clj
+++ project.clj
@@ -10,15 +10,12 @@
 ;;; You must not remove this notice, or any other, from this software.
 
 
-(defproject degel-clojure-utils "0.1.9"
+(defproject degel-clojure-utils "0.1.10"
   :description "A collection of Clojure utilities and extensions that I have found useful."
   :url "https://github.com/deg/degel-clojure-utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-
-  ;:repositories {"clojars" {:url "https://clojars.org"
-  ;                          :username "deg" :password :gpg}}
 
   :dependencies [;; Clojure itself
                  [org.clojure/clojure "1.5.1"]
@@ -36,36 +33,9 @@
                  [org.clojure/tools.nrepl "0.2.3"]
 
                  ;; For CLJS REPL
-                 [com.cemerick/piggieback "0.1.0"]
-                 ]
+                 [com.cemerick/piggieback "0.1.0"]]
 
   :profiles {:dev {:plugins [[lein-midje "2.0.4"]
                              [lein-marginalia "0.7.1"]]}}
 
-  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
-  :source-paths ["src/cljx"]
-  :resource-paths ["src/resources"]
-  :test-paths ["target/test-classes"]
-
-  :cljx {:builds [{:source-paths ["src/cljx"]
-                   :output-path "target/classes"
-                   :rules :clj}
-
-                  {:source-paths ["src/cljx"]
-                   :output-path "target/classes"
-                   :rules :cljs}
-
-                  {:source-paths ["test/cljx"]
-                   :output-path "target/test-classes"
-                   :rules :clj}
-
-                  {:source-paths ["test/cljx"]
-                   :output-path "target/test-classes"
-                   :rules :cljs}]}
-
-  :plugins [[lein-pprint "1.1.1"]
-            [com.keminglabs/cljx "0.3.0"]
-            ;[lein-pedantic "0.0.5"]
-            ]
-
-  :hooks [cljx.hooks])
+  :plugins [[lein-pprint "1.1.1"]])
